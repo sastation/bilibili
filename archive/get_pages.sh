8975af218a050943546097a72bf85d8bce1ea290
David Wang <sa.station@gmail.com>
2020-04-23 16:08:59 +0800

    remove redundant files

--- archive/get_pages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-url="http://search.bilibili.com/all?keyword=VOCALOID%E4%B8%AD%E6%96%87%E6%9B%B2&order=click&page="
-
-for num in {1..50}
-do
-    echo "$url$num"
-    curl -s -o page.$num "$url$num"
-done
--- archive/vc_menu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-~/src/danmaku2ass/danmaku2ass.py -o "./video/【言和翻唱】万神纪（feat. 中V全员）.ass" \
-    -s 1920x1080 -fn "微软雅黑" -fs 52 -a 0.8 -dm 12 -ds 6 \
-    "./video/【言和翻唱】万神纪（feat. 中V全员）.cmt.xml"
