048e172ccbb5a6bc4328b8e600b4f33c6c2abd05
David Wang <sa.station@gmail.com>
2023-01-31 13:47:19 +0800

    save mp3 files in ./audio/*

--- bb_mp3.py
+++ bb_mp3.py
@@ -15,8 +15,7 @@ def convert_mp3(video_dir='./video'):
     for av_file in lines:
         # 获取路径与文件名
         path, fname = os.path.split(av_file)
-
-        path = "%s/mp3" % path # 目录为 $video_dir/mp3
+        path = path.replace("video/", "audio/") # video目录改为audio
         mp3_file = "%s/%s.mp3" % (path, os.path.splitext(fname)[0]) # 文件为fname去除后缀加.mp3
 
         # 若 path 目录不存在则创建
@@ -25,6 +24,7 @@ def convert_mp3(video_dir='./video'):
         
         # 若 mp3_file 已存在则跳过
         if os.path.isfile(mp3_file):
+            print("%s is existed." % mp3_file)
             continue
 
         #print(mp3_file, os.path.exists(mp3_file))
