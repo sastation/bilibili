64f80948f38f0a94cde95996bda156e760d2fd6e
David Wang <sa.station@gmail.com>
2023-08-01 19:57:50 +0800

    mkdir folders if not exist

--- mount.sh
+++ mount.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+[[ -d audio ]] || mkdir audio
+[[ -d video ]] || mkdir video
+
 sudo mount --bind /mnt/ssd/pan/bilibili/video ./video
 sudo mount --bind /mnt/ssd/pan/bilibili/audio ./audio
